242325d1c4906abbb61a4c928cecac58803c9a74
sourabpramanik <shubpramanik241@gmail.com>
2024-07-17 07:33:53 +0530

    fix: use dynamic path and use algorthim specific config

---
+++ src/core/resources/oauth2_authorization.ts
@@ -0,0 +1,51 @@
+import * as oauth from 'oauth4webapi'
+import type { PayloadRequest } from 'payload/types'
+import { cookies } from 'next/headers'
+import type { ProviderClientConfig } from '../../types'
+import { getCallbackURL } from '../utils/cb'
+
+export async function OIDCAuthorization(
+  request: PayloadRequest,
+  provider: ProviderClientConfig,
+): Promise<Response> {
+  const callback_url = getCallbackURL(request)
+  const code_verifier = oauth.generateRandomCodeVerifier()
+  const code_challenge = await oauth.calculatePKCECodeChallenge(code_verifier)
+  const code_challenge_method = 'S256'
+
+  const client: oauth.Client = {
+    client_id: provider.client_id,
+    client_secret: provider.client_secret,
+    token_endpoint_auth_method: 'client_secret_basic',
+  }
+
+  const as = provider.authorization_server
+  const cookieMaxage = new Date(Date.now() + 300 * 1000)
+
+  const authorizationURL = new URL(as.authorization_endpoint!) // eslint-disable-line
+  authorizationURL.searchParams.set('client_id', client.client_id)
+  authorizationURL.searchParams.set('redirect_uri', callback_url.toString())
+  authorizationURL.searchParams.set('response_type', 'code')
+  authorizationURL.searchParams.set('scope', provider.scope as string)
+  authorizationURL.searchParams.set('code_challenge', code_challenge)
+  authorizationURL.searchParams.set('code_challenge_method', code_challenge_method)
+
+  if (provider.authorization_server.code_challenge_methods_supported?.includes('S256') !== true) {
+    const nonce = oauth.generateRandomNonce()
+    authorizationURL.searchParams.set('nonce', nonce)
+
+    cookies().set('payload_auth_nonce', nonce, {
+      expires: cookieMaxage,
+      path: '/',
+      httpOnly: true,
+    })
+  }
+
+  cookies().set('payload_auth_code_verifier', code_verifier, {
+    expires: cookieMaxage,
+    path: '/',
+    httpOnly: true,
+  })
+
+  return Response.redirect(authorizationURL.href)
+}
---
+++ src/core/resources/oidc_callback.ts
@@ -0,0 +1,75 @@
+import type { PayloadRequest } from 'payload/types'
+import { cookies } from 'next/headers'
+import * as oauth from 'oauth4webapi'
+import type { ProviderClientConfig } from '../../types'
+import { getCallbackURL } from '../utils/cb'
+
+export async function OIDCCallback(
+  request: PayloadRequest,
+  provider: ProviderClientConfig,
+): Promise<Response> {
+  const nonce = cookies().get('payload_auth_nonce')
+  const code_verifier = cookies().get('payload_auth_code_verifier')
+
+  if (!code_verifier) {
+    throw Error('Invalid session')
+  }
+
+  const client: oauth.Client = {
+    client_id: provider.client_id,
+    client_secret: provider.client_secret,
+    token_endpoint_auth_method: 'client_secret_basic',
+  }
+
+  const current_url = new URL(request.url as string)
+  const callback_url = getCallbackURL(request)
+  const as = provider.authorization_server
+
+  const params = oauth.validateAuthResponse(as, client, current_url)
+
+  if (oauth.isOAuth2Error(params)) {
+    throw new Error('Invalid params')
+  }
+
+  const response = await oauth.authorizationCodeGrantRequest(
+    as,
+    client,
+    params,
+    callback_url.toString(),
+    code_verifier.value,
+  )
+
+  const challenges = oauth.parseWwwAuthenticateChallenges(response)
+
+  if (challenges) {
+    for (const challenge of challenges) {
+      console.error('WWW-Authenticate Challenge', challenge) // eslint-disable-line
+    }
+
+    throw new Error('Failed to authenticate')
+  }
+
+  const token_result = await oauth.processAuthorizationCodeOpenIDResponse(
+    as,
+    client,
+    response,
+    nonce?.value as string,
+  )
+  if (oauth.isOAuth2Error(token_result)) {
+    throw new Error('Invalid response')
+  }
+  console.log(token_result)
+
+  // const session_url = new URL(request.url as string)
+  // session_url.pathname = session_url.pathname.replace(/\/callback\//, '/session/')
+  // session_url.host = request.headers.get('x-forwarded-host') || callback_url.host
+  // session_url.search = ''
+
+  // cookies().set('session-token', token_result.access_token as string, {
+  //   expires: new Date(Date.now() + 100 * 1000),
+  //   path: '/',
+  //   httpOnly: true,
+  // })
+
+  return Response.json('Session time')
+}
