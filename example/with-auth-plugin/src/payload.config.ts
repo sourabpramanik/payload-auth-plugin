e9f513d77c565b6b954e9fcd1a1b373730565e13
sourabpramanik <shubpramanik241@gmail.com>
2024-07-26 17:05:36 +0530

    feat: adds example app

---
+++ example/with-auth-plugin/src/payload.config.ts
@@ -0,0 +1,47 @@
+// storage-adapter-import-placeholder
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import path from 'path'
+import { buildConfig } from 'payload'
+import { fileURLToPath } from 'url'
+import sharp from 'sharp'
+
+import { Users } from './collections/Users'
+import { Media } from './collections/Media'
+
+import AuthPlugin from '../../../dist/src'
+import { GoogleAuthProvider, DiscordAuthProvider } from '../../../dist/src/providers'
+import '../../../dist/src/index.css'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+  },
+  collections: [Users, Media],
+  editor: lexicalEditor(),
+  secret: process.env.PAYLOAD_SECRET || '',
+  typescript: {
+    outputFile: path.resolve(dirname, 'payload-types.ts'),
+  },
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URI || '',
+  }),
+  sharp,
+  plugins: [
+    AuthPlugin({
+      providers: [
+        GoogleAuthProvider({
+          client_id: process.env.GOOGLE_CLIENT_ID as string,
+          client_secret: process.env.GOOGLE_CLIENT_SECRET as string,
+        }),
+        DiscordAuthProvider({
+          client_id: process.env.DISCORD_CLIENT_ID as string,
+          client_secret: process.env.DISCORD_CLIENT_SECRET as string,
+        }),
+      ],
+    }),
+  ],
+})
